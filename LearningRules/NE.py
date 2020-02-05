8779d07b493d2696a77df32191ea35b4354c4412
Roland Mühlenbernd <muehlenbernd@web.de>
2020-02-05 13:19:21 +0100

    Create NE.py
    Nash equilibrium (NE) computation

---
+++ LearningRules/NE.py
@@ -0,0 +1,47 @@
+import MyMath as MyMath
+
+class NE:
+
+    ### CONSTRUCTOR ###
+
+    def __init__(self):
+
+        self.prob_coop = 0.5
+
+
+    ### CHOICE FUNCTION ###
+
+    def choice(self, game):
+
+        if game.type == 'PD':
+            self.prob_coop = 0.0
+
+        elif game.type == 'SH':
+
+            a = game.payoffs[0][0]
+            b = game.payoffs[0][1]
+            c = game.payoffs[1][0]
+            d = game.payoffs[1][1]
+
+            self.prob_coop = (d - b) * 1.0 / (a - b - c + d)
+
+        if MyMath.prob_choice(self.prob_coop):
+            choice = 'C'
+        else:
+            choice = 'D'
+
+        return choice
+
+
+    ### UPDATE FUNCTION ###
+
+    def update(self, game, my_choice, opponent_choice):
+
+        self.prob_coop = 0.5
+
+
+    ### RESET FUNCTION ###
+
+    def reset(self):
+
+        self.prob_coop = 0.5
