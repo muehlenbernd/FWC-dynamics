437ff8c8514c3be622e5c05bf6c39598d2788bb3
Roland Mühlenbernd <muehlenbernd@web.de>
2020-02-05 13:09:17 +0100

    Create __init__.py
    Subfolder of teh different learning rules

---
+++ LearningRules/__init__.py
@@ -0,0 +1,6 @@
+from LearningRules.CF import *
+from LearningRules.EWA import *
+from LearningRules.FP import *
+from LearningRules.FWC import *
+from LearningRules.NE import *
+from LearningRules.RL import *
